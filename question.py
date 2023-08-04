d6c956f6b3b77a2255874996e65f9619cc77e85b
pelsingunduz <pelsingunduz@icloud.com>
2023-08-04 03:37:36 +0300

    Comment line fixes

--- question.py
+++ question.py
@@ -1,16 +1,18 @@
-input = input("Bir dize girin: ")
+input = input("Enter a string: ")
 
-longest_substring = "" #en uzun tekrarsız alt dizeyi tutacak değişken
-current_substring = "" #mevcut alt dizeyi geçici olarak tutacak değişken 
+longest_substring = "" #variable to hold the longest non-repeating substring
+current_substring = "" #variable to temporarily store the current substring
 
-for character in input: #dizeyi karakter karakter gezecek döngü 
+#loop to iterate over the string character by character
+for character in input: 
     if character in current_substring:
         index = current_substring.index(character)
         current_substring = current_substring[index + 1:]
     current_substring += character
 
+   #if the length of the current substring is longer than the length of the longest non-repeating substring, the longest non-repeating substring is updated
     if len(current_substring) > len(longest_substring):
         longest_substring = current_substring
 
-print("En uzun yinelemesiz alt dize:", longest_substring)
-print("Uzunluk:", len(longest_substring))
+print("Longest substring without duplicates:", longest_substring)
+print("Length", len(longest_substring))
